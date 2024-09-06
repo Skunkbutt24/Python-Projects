ef167a63ecd8e634936f5147f924e3873032a7c0
Liman <156384174+Skunkbutt24@users.noreply.github.com>
2024-09-05 21:39:40 -0400

    Add files via upload

---
+++ Day 6/Shopping Cart.py
@@ -0,0 +1,38 @@
+logo = '''
+⠀⠈⠛⠻⠶⣶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⢻⣆⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢻⡏⠉⠉⠉⠉⢹⡏⠉⠉⠉⠉⣿⠉⠉⠉⠉⠉⣹⠇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⣿⣀⣀⣀⣀⣸⣧⣀⣀⣀⣀⣿⣄⣀⣀⣀⣠⡿⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠸⣧⠀⠀⠀⢸⡇⠀⠀⠀⠀⣿⠁⠀⠀⠀⣿⠃⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣧⣤⣤⣼⣧⣤⣤⣤⣤⣿⣤⣤⣤⣼⡏⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⠀⠀⢸⡇⠀⠀⠀⠀⣿⠀⠀⢠⡿⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣷⠤⠼⠷⠤⠤⠤⠤⠿⠦⠤⠾⠃⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢾⣷⢶⣶⠶⠶⠶⠶⠶⠶⣶⠶⣶⡶⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣧⣠⡿⠀⠀⠀⠀⠀⠀⢷⣄⣼⠇⠀⠀⠀⠀⠀⠀⠀
+'''
+print(logo)
+print("Welcome to my virtual shopping cart program. Let's get started.")
+foods = []
+prices = []
+total_price = 0
+number_of_items=0
+while True:
+    food = input("Enter the name of food you want to store in cart: ").lower()
+    foods.append(food)
+    price = float(input(f"Enter the price of {food.capitalize()}: $ "))
+    prices.append(price)
+    continue_adding = input("Do you want to add more stuffs ? ").lower()
+    if continue_adding == "no":
+        break
+print("So, your shopping cart contains: ")
+n=1
+for food in foods:
+    print(f"{n}:{food.capitalize()}")
+    n+=1
+for price in prices:
+    total_price += price
+    number_of_items+=1
+print(f"Number of items in cart: {number_of_items}")
+print("Tax percentage: 4%")
+print(f"Total price with tax: ${(total_price+(total_price*(4/100))):.2f}")
